85e39d3b5e62bb8a2f43b099a393d1a2ea796341
Masrik <dahirma@vcu.edu>
2021-10-06 18:49:55 -0400

    final

--- Project 2/src/Generate.java
+++ Project 2/src/Generate.java
@@ -7,7 +7,7 @@ public class Generate {
         int rand = (int)(Math.random()*(99999)+2);
         String f = String.valueOf(rand) + "\n";
         for (int i = 0; i < rand; i++){
-            f += (int)(Math.random()*(100)+1) + " " + (int)(Math.random()*(3)+1) + "\n";
+            f += (int)(Math.random()*(1000000000)+1) + " " + (int)(Math.random()*(3)+1) + "\n";
         }
         File file = new File("Project 2/src/filename.txt");
         FileWriter myWriter = new FileWriter("Project 2/src/filename.txt");
--- Project 2/src/Test.java
+++ Project 2/src/Test.java
@@ -1,7 +1,5 @@
 import java.util.*;
 
-import static java.lang.Math.abs;
-
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
