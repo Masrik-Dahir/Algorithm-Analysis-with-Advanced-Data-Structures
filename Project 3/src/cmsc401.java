2b6af53cd52d6caa9250918f27f94d61fc9e7959
Masrik Dahir <dahirma@vcu.edu>
2021-11-18 00:15:34 -0500

    new commit

--- Project 3/src/cmsc401.java
+++ Project 3/src/cmsc401.java
@@ -1,20 +1,7 @@
+//Masrik Dahir
 import java.util.Scanner;
 
 public class cmsc401{
-    static boolean Assign_course_to_room(boolean[][] check, int ind,
-                                         boolean[] recognized, int[] size,
-                                         int count_C, int count_H){
-        for(int i = 0; i<count_H; i++){
-            if(check[ind][i] && !recognized[i]){
-                recognized[i] = true;
-                if(size[i] < 0 || Assign_course_to_room(check, size[i], recognized, size, count_C, count_H)) {
-                    size[i] = ind;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 
     static int max_flow_match(boolean[][] check, int count_C, int count_H){
         int[] flow_recognized = new int[count_H];
@@ -34,7 +21,6 @@ public class cmsc401{
         return increment;
     }
 
-
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int count_C = Integer.parseInt(input.nextLine());
@@ -51,5 +37,20 @@ public class cmsc401{
         }
         int answer = max_flow_match(check,count_C,count_H);
         System.out.println(answer);
+    }
+
+    static boolean Assign_course_to_room(boolean[][] check, int ind,
+                                         boolean[] recognized, int[] size,
+                                         int count_C, int count_H){
+        for(int i = 0; i<count_H; i++){
+            if(check[ind][i] && !recognized[i]){
+                recognized[i] = true;
+                if(size[i] < 0 || Assign_course_to_room(check, size[i], recognized, size, count_C, count_H)) {
+                    size[i] = ind;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
