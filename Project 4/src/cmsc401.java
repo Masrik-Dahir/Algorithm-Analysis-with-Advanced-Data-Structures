b1d92e05447eea923bfa16e4441b34bb76d4e9a1
Masrik Dahir <dahirma@vcu.edu>
2021-11-29 10:16:48 -0500

    generate -> sorted

--- Project 4/src/cmsc401.java
+++ Project 4/src/cmsc401.java
@@ -6,117 +6,75 @@ import java.util.Arrays;
 public class cmsc401 {
 
     static class Rod {
-        private int rodSize;
-        private int numCutPoints;
-        private int[] m;
-        private int[][] dp;
+        private int _size;
+        private int _cuts;
+        private final int[] single_array;
+        private final int[][] double_array;
 
-        public Rod(int rodSize, int numCutPoints) {
-            setRodSize(rodSize);
-            setNumCutPoints(numCutPoints);
+        public Rod(int _size, int _cuts) {
+            set_size(_size);
+            set_cuts(_cuts);
+            double_array = new int[_cuts][_cuts];
+            this.single_array = new int[_cuts];
 
-            dp = new int[numCutPoints][numCutPoints];
-            this.m = new int[numCutPoints];
-
-            for (int i = 0; i < dp.length; i++) {
-                Arrays.fill(dp[i], -1);
+            for (int[] ints : double_array) {
+                Arrays.fill(ints, -1);
             }
         }
 
-        public void setRodSize(int rodSize) {
-            this.rodSize = rodSize;
+        public void set_size(int _size) {
+            this._size = _size;
         }
 
-        public void setNumCutPoints(int numCutPoints) {
-            this.numCutPoints = numCutPoints;
+        public void set_cuts(int _cuts) {
+            this._cuts = _cuts;
         }
 
-        public void addCutPoint(int i, int cutPoint) {
-            m[i] = cutPoint;
+        public void add(int i, int cutPoint) {
+            single_array[i] = cutPoint;
 //            quickSort(m,0, m.length - 1);
         }
 
-        public int getNumCutPoints() {
-            return numCutPoints;
+        public int get_cuts() {
+            return _cuts;
         }
 
-        public int minimumCost() {
-            return minimumCost(0, numCutPoints-1, 0, rodSize);
+        public int lowest_cost() {
+            return lowest_cost(0, _cuts -1, 0, _size);
         }
 
-        public int minimumCost(int leftCutIndex, int rightCutIndex, int segmentStart, int segmentEnd) {
+        public int lowest_cost(int leftCutIndex, int rightCutIndex, int segmentStart, int segmentEnd) {
 
-            if (rightCutIndex >= numCutPoints || leftCutIndex > rightCutIndex)
+            if (rightCutIndex >= _cuts || leftCutIndex > rightCutIndex)
                 return 0;
 
             if (leftCutIndex == rightCutIndex)
                 return segmentEnd - segmentStart;
 
-            if (dp[leftCutIndex][rightCutIndex] != -1)
-                return dp[leftCutIndex][rightCutIndex];
+            if (double_array[leftCutIndex][rightCutIndex] != -1)
+                return double_array[leftCutIndex][rightCutIndex];
 
             int currentMin = Integer.MAX_VALUE;
 
             for(int i = leftCutIndex; i <= rightCutIndex; i++)
-                currentMin = Math.min(segmentEnd-segmentStart + minimumCost(leftCutIndex, i-1, segmentStart, m[i]) + minimumCost(i+1, rightCutIndex, m[i], segmentEnd), currentMin);
-            dp[leftCutIndex][rightCutIndex] = currentMin;
+                currentMin = Math.min(segmentEnd-segmentStart + lowest_cost(leftCutIndex, i-1, segmentStart, single_array[i]) + lowest_cost(i+1, rightCutIndex, single_array[i], segmentEnd), currentMin);
+            double_array[leftCutIndex][rightCutIndex] = currentMin;
 
             return currentMin;
         }
-
-        static void swap(int[] arr, int i, int j)
-        {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-
-
-//        static int partition(int[] arr, int low, int high)
-//        {
-//
-//            int pivot = arr[high];
-//            int i = (low - 1);
-//
-//            for(int j = low; j <= high - 1; j++)
-//            {
-//                if (arr[j] < pivot)
-//                {
-//                    i++;
-//                    swap(arr, i, j);
-//                }
-//            }
-//            swap(arr, i + 1, high);
-//            return (i + 1);
-//        }
-//
-//        static void quickSort(int[] arr, int low, int high)
-//        {
-//            if (low < high)
-//            {
-//
-//                int pi = partition(arr, low, high);
-//                quickSort(arr, low, pi - 1);
-//                quickSort(arr, pi + 1, high);
-//            }
-//        }
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int rodSize = in.nextInt();
+        int _size = input.nextInt();
+        int _cuts = input.nextInt();
+        Rod first = new Rod(_size, _cuts);
 
-        int m = in.nextInt();
+        for (int i = 0; i < first.get_cuts(); i++)
+            first.add(i, input.nextInt());
 
-        Rod rod = new Rod(rodSize, m);
-
-        for (int i = 0; i < rod.getNumCutPoints(); i++)
-            rod.addCutPoint(i, in.nextInt());
-
-        in.close();
-
-
-        System.out.println(rod.minimumCost());
+        input.close();
+        System.out.println(first.lowest_cost());
     }
 }
